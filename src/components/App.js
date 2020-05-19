a0d501dcc5ae0649a9dc8ddcdb7de13a1f81e3f2
Abhishek Kumar Sharma <abhi243461@hotmail.com>
2020-05-19 13:59:00 +0530

    feat: Add actions, middleware, reducers

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { handleInitialData } from '../actions/shared'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello React</h1>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>khbcdhbc</h1>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect()(App);
